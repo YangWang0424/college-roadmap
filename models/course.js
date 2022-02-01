c0dca300b7b4abd7b291c1aeafbca526e31fc531
YangWang0424 <wangya4@oregonstate.edu>
2022-02-01 12:42:50 +0800

    copy from main

---
+++ models/course.js
@@ -0,0 +1,16 @@
+// Require Mongoose
+const mongoose = require('mongoose');
+
+// 定义一个模式
+const Schema = mongoose.Schema;
+
+const CourseSchema = new Schema({
+    name:String,
+    desc:String,
+    major : { type: Schema.Types.ObjectId, ref: 'Major' },
+    users:[{type: Schema.Types.ObjectId, ref: 'User'}]
+
+});
+
+// 导出函数来创建 "SomeModel" 模型类
+module.exports = mongoose.model('Course', CourseSchema);
